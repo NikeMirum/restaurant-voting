7961445852d19d68253ec0eeab0e623350bec69e
NikeMirum <75948964+NikeMirum@users.noreply.github.com>
2021-09-02 15:27:50 +0300

    Init: 3 new Repositories added (beta) with checking in RestaurantVotingApplication

--- src/main/java/ru/javaops/topjava2/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/topjava2/RestaurantVotingApplication.java
@@ -1,12 +1,31 @@
 package ru.javaops.topjava2;
 
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.javaops.topjava2.model.Restaurant;
+import ru.javaops.topjava2.repository.RestaurantRepository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 @SpringBootApplication
-public class RestaurantVotingApplication {
+@AllArgsConstructor
+public class RestaurantVotingApplication implements ApplicationRunner {
+    private final RestaurantRepository restaurantRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantVotingApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(LocalDate.now());
+        ArrayList<Restaurant> restaurants = (ArrayList<Restaurant>) restaurantRepository.getAllWithDish(LocalDate.now());
+        for (Restaurant restaurant : restaurants) {
+            System.out.println(restaurant);
+        }
     }
 }
