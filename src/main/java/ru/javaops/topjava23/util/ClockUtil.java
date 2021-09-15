eaa5ceee2705fc50ac3807500a70b53808d6eabe
NikeMirum <75948964+NikeMirum@users.noreply.github.com>
2021-09-15 16:52:07 +0300

    Update: Switching ClockUtil back to normal mode

--- src/main/java/ru/javaops/topjava23/util/ClockUtil.java
+++ src/main/java/ru/javaops/topjava23/util/ClockUtil.java
@@ -6,12 +6,11 @@ import java.time.*;
 
 @UtilityClass
 public class ClockUtil {
-    public static Clock clock = Clock.fixed(Instant.ofEpochSecond(LocalDate.now().atStartOfDay()
-            .toEpochSecond(ZoneOffset.UTC)), ZoneId.systemDefault());
+    public static Clock clock = Clock.systemDefaultZone();
 
     public static void useMockClock() {
-        clock = Clock.fixed(Instant.ofEpochSecond(LocalDate.now().atStartOfDay()
-                .toEpochSecond(ZoneOffset.UTC)), ZoneId.systemDefault());
+        clock = Clock.fixed((LocalDateTime.of(LocalDate.now(), LocalTime.of(9, 0))
+                .toInstant(OffsetDateTime.now().getOffset())), Clock.systemDefaultZone().getZone());
     }
 
     public static void useDefaultClock() {
